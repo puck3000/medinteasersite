7e774f0d91c0eecd67ba7c0a8474a4166b5d1f84
Daniel Lerch <daniel@lerchpanther.ch>
2019-08-26 10:09:46 +0200

    added meta tags

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,46 @@ export default {
     Video,
     Content,
     Foot
+  },
+  metaInfo: {
+    meta: [
+      // OpenGraph data (Most widely used)
+      { property: "og:title", content: "Medin Biel Bienne | Teasersite" },
+      { property: "og:site_name", content: "Medin Biel Bienne | Teasersite" },
+      // The list of types is available here: http://ogp.me/#types
+      { property: "og:type", content: "website" },
+      // Should the the same as your canonical link, see below.
+      { property: "og:url", content: "https://medinteasersite.netlify.com/" },
+      { property: "og:image", content: "@/assets/img/Poster.jpg" },
+      // Often the same as your meta description, but not always.
+      {
+        property: "og:description",
+        content: "Teaserseite für Medin Biel/Bienne | ab Oktober 2019"
+      },
+
+      // Twitter card
+      {
+        name: "twitter:card",
+        content: "Teaserseite für Medin Biel/Bienne | ab Oktober 2019"
+      },
+      { name: "twitter:site", content: "https://medinteasersite.netlify.com/" },
+      { name: "twitter:title", content: "Medin Biel Bienne | Teasersite" },
+      {
+        name: "twitter:description",
+        content: "Teaserseite für Medin Biel/Bienne | ab Oktober 2019"
+      },
+      // Your twitter handle, if you have one.
+      { name: "twitter:creator", content: "@medinbielbienne" },
+      { name: "twitter:image:src", content: "@/assets/img/Poster.jpg" },
+
+      // Google / Schema.org markup:
+      { itemprop: "name", content: "Medin Biel Bienne | Teasersite" },
+      {
+        itemprop: "description",
+        content: "Teaserseite für Medin Biel/Bienne | ab Oktober 2019"
+      },
+      { itemprop: "image", content: "@/assets/img/Poster.jpg" }
+    ]
   }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
 
+import Meta from 'vue-meta';
+
+Vue.use(Meta);
+
 Vue.config.productionTip = false;
 
 new Vue({
